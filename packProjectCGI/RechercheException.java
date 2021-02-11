7d1947ea71acd198a8f8d9e0466c87ee5bf3773a
Léa <38650627+am3thyste@users.noreply.github.com>
2021-02-11 20:02:39 +0100

    Delete RechercheException.java

--- packProjectCGI/RechercheException.java
+++
@@ -1,7 +0,0 @@
-//package packProjectCGI;
-//
-//public class RechercheException extends Exception{
-//	public RechercheException(){
-//		System.out.println("Résultat de la recherche professeur/e: le professeur n'existe pas dans la base");
-//	}
-//}
